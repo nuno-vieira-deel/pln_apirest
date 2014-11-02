2d7f4d06899af342206f0d2fd8a5e3f93901f0df
Nuno Vieira <nunovieira220@gmail.com>
2014-11-02 11:56:10 -0800

    Insercao da param_function no hash e no modulo e autoloading dos modulos todos da familia dos serviços

--- lib/ws.pm
+++ lib/ws.pm
@@ -1,21 +1,35 @@
 package ws;
-use Dancer2 ':syntax';
+package API_PLN::Service;
 
+use Dancer2 ':syntax';
 use strict;
 use warnings;
 use Data::Dumper;
 use Module::Load;
 use Import::Into;
+use Class::Factory::Util;
+
+my @modulelist = API_PLN::Service->subclasses;
+
+for my $module (@modulelist){
+  autoload "API_PLN::Service::".$module;
+}
 
 
 our $VERSION = '0.1';
 
 my %routemap = (
-  'tokenizer' => sub {
-      my ($text) = @_;
-      autoload API_PLN::Service::TokenizerFL3;
-      my $result = to_json main_function($text);
-      return $result;
+  'tokenizer' => {
+      param_function => sub {
+        my (%hparams) = @_;
+        #funcao_teste();
+        return 1;
+      },
+      main_function => sub {
+        my ($text) = @_;
+        my $result = to_json main_function($text);
+        return $result;
+      }
     }
 );
 
@@ -25,10 +39,10 @@ get '/' => sub {
 };
 
 any ['get', 'post'] => '/*' => sub {
-  my ($path) = splat;
-  #my $text = param 'text'; 
-  my $text = "Eu sou o Nuno.";
-  my $result = $routemap{$path}->($text);
+  my ($path) = splat; 
+  my $val = $routemap{$path}{param_function}->(params);
+  my $text = param 'text';
+  my $result = $routemap{$path}{main_function}->($text);
   template 'index' => { res => $result };
 };
 
