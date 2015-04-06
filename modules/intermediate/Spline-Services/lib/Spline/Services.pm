88bbc9ae16d4aef66affb28d5e2f28cc7d40b40a
Nuno Vieira <nunovieira220@gmail.com>
2015-04-06 09:39:04 -0700

    test do modulo services

--- modules/intermediate/Spline-Services/lib/Spline/Services.pm
+++ modules/intermediate/Spline-Services/lib/Spline/Services.pm
@@ -9,24 +9,11 @@ use URI::Escape;
 use JSON;
 use Sub::Install;
 
-require Exporter;
-our @ISA = qw(Exporter);
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-our @EXPORT = qw(
-	&import
-);
-our $VERSION = '0.01';
-
 sub import {
 	my $url = "http://localhost:8080/info";
 
 	my $response = HTTP::Tiny->new->get($url);
-	if (! $response->{success}) {
-		die "$response->{status}: $response->{reason}";
-	}
+	die if (!$response->{success});
 
 	my $data_structure = decode_json($response->{content});
 
@@ -34,7 +21,6 @@ sub import {
 		Sub::Install::install_sub({
 			code => sub {
 				my %params = @_;
-				print Dumper(\%params);
 				## tv seja preciso converter alguns parametros do %param para JSON, etc.
 				my $res = HTTP::Tiny->new->post_form("http://localhost:8080/".$method, \%params);
 				die "$res->{status}: $res->{reason}" unless $response->{success};
